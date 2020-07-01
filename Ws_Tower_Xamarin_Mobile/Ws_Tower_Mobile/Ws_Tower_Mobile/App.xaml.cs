bf47bc4501b6ec2690cf4106563b4d814dfc820c
Rodhenrique <henriquesilvasenai@gmail.com>
2020-07-01 11:28:11 -0300

    Interfaces

---
+++ Ws_Tower_Xamarin_Mobile/Ws_Tower_Mobile/Ws_Tower_Mobile/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Ws_Tower_Mobile.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ws_Tower_Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new FrmSplash();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
